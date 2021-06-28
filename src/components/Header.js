45310ecf88cf40c7d0972488097f855c2799bd50
OdehYazan <eng.yazanodeh@gmail.com>
2021-06-29 01:03:27 +0300

    lab01

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class Header extends React.Component{
+    render(){
+        return(
+            <div className='head'>
+                <h1>Lab: Class 01</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
