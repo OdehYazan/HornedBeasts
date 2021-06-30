95d6c3eeed563581115ad46fad0cb765cb58faa4
OdehYazan <eng.yazanodeh@gmail.com>
2021-06-30 23:06:31 +0300

    need filter

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HornedBeasts from './HornedBeasts';
 import Data from './Data.json'
 import Model from './Model';
+import {Form,Button} from 'react-bootstrap'
 class Main extends React.Component {
 
     constructor(props) {
@@ -9,16 +10,19 @@ class Main extends React.Component {
         this.state = {
             modelOn: false,
             imgUrl: '',
-            title:'',
-            description:''
+            title: '',
+            description: '',
+            numberOfHorns: 0,
+            newArray:[]
         }
     }
-    setData = (img,title,description) => {
+    setData = (img, title, description) => {
         this.setState({
             imgUrl: img,
-            title:title,
-            description:description,
+            title: title,
+            description: description,
             modelOn: true,
+
         })
     }
     // showModel = () => {
@@ -34,23 +38,61 @@ class Main extends React.Component {
         })
     }
 
+    submit = (event) => {
+        event.preventDefault();
+        let data 
+        this.setState({
+            numberOfHorns: event.target.numberOfHorns.value
+        })
+        
+         Data.filter(item =>{
+             data = item.horns === this.state.numberOfHorns
+            return({
+                data
+            } )
+        })
+        console.log(data);
+    }
+
 
     render() {
         return (
             <div >
-                {Data.map((item, index) => {
-                    return (
 
-                        <HornedBeasts imgUrl={item.image_url} title={item.title} description={item.description} key={index} setData={this.setData} />
 
-                    )
-                })
+                {<form onSubmit={this.submit}>
+
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Label>Filter Beasts By Horns Number </Form.Label>
+                        <Form.Control as="select" name='numberOfHorns' type='submit'>
+                            <option value='0'>All</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='100'>😱😱</option>
+                        </Form.Control>
+                        <Button variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </Form.Group>
+
+
+                </form>
+
                 }
+                {/* {
+                    Data.map((item, index) => {
+                        return (
 
+                            <HornedBeasts imgUrl={item.image_url} title={item.title} description={item.description} key={index} setData={this.setData} />
 
+                        )
+                    })
+                } */}
+                  
 
                 {
-                   
+
                     <Model show={this.state.modelOn} onHide={this.hideModel} img={this.state.imgUrl} title={this.state.title} description={this.state.description} />
                 }
 
@@ -59,7 +101,8 @@ class Main extends React.Component {
 
 
 
-            </div>
+
+            </div >
 
 
         )
